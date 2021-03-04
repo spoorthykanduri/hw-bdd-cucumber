15808858076e2636e23be23e1217e41d351732a9
spoorthykanduri <spoorthy.kanduri@tamu.edu>
2021-03-04 02:51:41 +0000

    final commit

--- rottenpotatoes/features/filter_movie_list.feature
+++ rottenpotatoes/features/filter_movie_list.feature
@@ -12,7 +12,7 @@ Background: movies have been added to database
   | The Terminator          | R      | 26-Oct-1984  |
   | When Harry Met Sally    | R      | 21-Jul-1989  |
   | The Help                | PG-13  | 10-Aug-2011  |
-  | Chocolat                | PG-13  | 5-Jan-2001   |
+  | Chocolat            | PG-13  | 5-Jan-2001   |
   | Amelie                  | R      | 25-Apr-2001  |
   | 2001: A Space Odyssey   | G      | 6-Apr-1968   |
   | The Incredibles         | PG     | 5-Nov-2004   |
@@ -22,8 +22,25 @@ Background: movies have been added to database
   And  I am on the RottenPotatoes home page
   Then 10 seed movies should exist
 
-Scenario: restrict to movies with 'PG' or 'R' ratings
-  # enter step(s) to check the 'PG' and 'R' checkboxes
+Scenario: restrict to movies with "PG" or "R" ratings
+  When I check the following ratings: PG, R
+  And I uncheck the following ratings: G, NC-17, PG-13
+  And I press "Refresh"
+  Then I should see "The Terminator"
+  And I should see "When Harry Met Sally"
+  And I should see "Amelie"
+  And I should see "The Incredibles"
+  And I should see "Raiders of the Lost Ark"
+  And I should not see "The Help"
+  And I should not see "Aladdin"
+  And I should not see " Chocolat"
+  And I should not see "2001: A Space Odyssey"
+  And I should not see "Chicken Run"
+  # Then I should see all movies with ratings: PG,R
+  # And I should not see movies with ratings: G,NC-17,PG-13
+  
+  
+  # enter step(s) to check the "PG" and "R" checkboxes
   # enter step(s) to uncheck all other checkboxes
   # enter step to "submit" the search form on the homepage
   # enter step(s) to ensure that PG and R movies are visible
@@ -31,3 +48,6 @@ Scenario: restrict to movies with 'PG' or 'R' ratings
 
 Scenario: all ratings selected
   # see assignment
+  When I check the following ratings: G, PG, R, NC-17, PG-13
+  And I press "Refresh"
+  Then I should see all the movies
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -3,9 +3,13 @@
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
+  Movie.create(title: movie[:title],
+                 rating: movie[:rating],
+                 release_date: movie[:release_date],
+                )
     # you should arrange to add that movie to the database here.
   end
-  fail "Unimplemented"
+  
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -18,7 +22,9 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  page.body.should match(/.*#{e1}.*#{e2}/m)
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -29,10 +35,37 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.split(", ").each do |rating|
+    if uncheck
+      step %{I uncheck "ratings_#{rating}"}
+    else
+      step %{I check "ratings_#{rating}"}
+    end
+  end
 end
+
+# Then /I should see all movies with ratings: (.*)/ do |rating_list|
+#   ratings = page.all('#movies tbody tr td:nth-child(2)').map(&:text)
+#   rating_list.split(',').each do |rating|
+#     expect(ratings).to have_content(rating)
+#   end
+# end
+
+# And /^(?:|I )press "([^"]*)"$/ do |button|
+#   click_button(Refresh)
+# end
+
+# Then /^(?:|I )should see "([^"]*)"$/ do |string|
+#   if page.respond_to? :should
+#     page.should have_content(The Terminator)
+#   else
+#     assert page.has_content?(The Terminator)
+#   end
+# end
+
+
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  page.all('table#movies tr').count.should == Movie.count+1
 end
